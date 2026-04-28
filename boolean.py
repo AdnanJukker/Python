6d25e8106b725ead24710f921025867930e7725e
Adnan Jukker <93384175+AdnanJukker@users.noreply.github.com>
2026-04-28 23:17:46 +0530

    Add comments to boolean expressions for clarity

--- boolean.py
+++ boolean.py
@@ -3,10 +3,10 @@ print(10 == 9)
 print(10 < 9)
 
 # Print the result of 10 > 9
-print(10 > 9)
+print(10 > 9) #True
 # Print the result of 10 == 9
-print(10 == 9)
+print(10 == 9) #False
 # Print the result of bool("Hello")
-print(bool("Hello"))
+print(bool("Hello")) #True
 # Print the result of bool(0)
-print(bool(0))
+print(bool(0)) #False
