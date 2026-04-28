7727a656b1a41e26e00dd9e360dd68b25006264a
Adnan Jukkerwala <adnanjukker402@gmail.com>
2026-04-28 22:51:17 +0530

    f-strings.py added

---
+++ f-strings.py
@@ -0,0 +1,22 @@
+age = 36 
+txt = f"My name is XYZ , iam {age}"
+print(txt)
+
+txt = f"The price is {30*40} Rupees"
+print(txt)
+
+price = 59
+txt = f"The price is {price:.2f} rupees"
+print(txt)
+
+"""
+\' 	Single Quote 	
+\\ 	Backslash 	
+\n 	New Line 	
+\r 	Carriage Return 	
+\t 	Tab 	
+\b 	Backspace 	
+\f 	Form Feed 	
+\ooo 	Octal value 	
+\xhh 	Hex value
+"""
