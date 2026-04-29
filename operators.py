a4a947f92d4dc7b98b365cdd2c68cf6b35fa07de
Adnan Jukkerwala <adnanjukker402@gmail.com>
2026-04-29 08:51:14 +0530

    arithmetic operators

---
+++ operators.py
@@ -0,0 +1,26 @@
+"""ARITHMETIC OPERATORS"""
+"""
++ 	Addition 	x + y 	
+- 	Subtraction 	x - y 	
+* 	Multiplication 	x * y 	
+/ 	Division 	x / y 	
+% 	Modulus 	x % y 	
+** 	Exponentiation 	x ** y 	
+// 	Floor division 	x // y
+"""
+
+sum1 = 100 + 50      # 150 (100 + 50)
+sum2 = sum1 + 250    # 400 (150 + 250)
+sum3 = sum2 + sum2   # 800 (400 + 400) 
+print(sum1,sum2,sum3)
+
+x = 15
+y = 4
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
