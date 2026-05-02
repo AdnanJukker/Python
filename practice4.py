70c2682a37efae9183a24b22b1fc94151160e585
Adnan Jukkerwala <adnanjukker402@gmail.com>
2026-05-02 21:30:14 +0530

    practice of list and tuples

---
+++ practice4.py
@@ -0,0 +1,44 @@
+fruits = []
+
+f1 = input("Enter fruits 1 :")
+fruits.append(f1)
+f2 = input("Enter fruits 2 :")
+fruits.append(f2)
+f3 = input("Enter fruits 3 :")
+fruits.append(f3)
+f4 = input("Enter fruits 4 :")
+fruits.append(f4)
+f5 = input("Enter fruits 5 :")
+fruits.append(f5)
+f6 = input("Enter fruits 6 :")
+fruits.append(f6)
+f7 = input("Enter fruits 7 :")
+fruits.append(f7)
+print(fruits)
+
+marks = []
+
+f1 = int(input("Enter marks 1 :"))
+marks.append(f1)
+f2 = int(input("Enter marks 2 :"))
+marks.append(f2)
+f3 = int(input("Enter marks 3 :"))
+marks.append(f3)
+f4 = int(input("Enter marks 4 :"))
+marks.append(f4)
+f5 = int(input("Enter marks 5 :"))
+marks.append(f5)
+f6 = int(input("Enter marks 6 :"))
+marks.append(f6)
+marks.sort()
+print(marks) 
+
+l1 = (1,2,3,4,5,6,7,8,9)
+l1[7]= 10
+print(l1)
+
+l1 = (1,2,3,4,5,6,7,8,9)
+print(sum(l1))
+
+a = (7,0,8,0,0,9)
+print(a.count(0))
