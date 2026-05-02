a671308b88eb5341c552e1b1d6ee0a941879600e
Adnan Jukkerwala <adnanjukker402@gmail.com>
2026-05-02 16:32:06 +0530

    functions Complete

--- functions.py
+++ functions.py
@@ -89,3 +89,143 @@ def my_function(name, /):
   print("Hello", name)
 
 my_function("Emil")
+
+"""The *args parameter allows a function to accept any number of positional arguments (only for tuples)"""
+
+def my_function(*kids): 
+  print("The youngest child is " + kids[1])
+
+my_function("Emil", "Tobias", "Linus") 
+
+def my_function(*args):
+  print("type:",type(args))
+  print("First argument",args[0])
+  print("Second argument",args[1])
+  print("All arguments",args)
+
+my_function("Emil","Tobias","Linus")
+
+
+def my_function(greeting, *names):
+  for name in names:
+    print(greeting, name)
+
+my_function("Hello", "Emil", "Tobias", "Linus") 
+
+def my_function(*numbers):
+  total = 0
+  for num in numbers:
+    total += num
+  return total
+
+print(my_function(1, 2, 3))
+print(my_function(10, 20, 30, 40))
+print(my_function(5)) 
+
+
+def my_function(*numbers):
+  if len(numbers) == 0:
+    return None
+  max_num = numbers[0]
+  for num in numbers:
+    if num > max_num:
+      max_num = num
+  return max_num
+
+print(my_function(3, 7, 2, 9, 1))
+
+"""If you do not know how many keyword arguments will be passed into your function, add two asterisks ** before the parameter name. only for dict"""
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
+
+my_function(fname = "Tobias", lname = "Refsnes") 
+
+def my_function(**myvar):
+  print("Type:", type(myvar))
+  print("Name:", myvar["name"])
+  print("Age:", myvar["age"])
+  print("All data:", myvar)
+
+my_function(name = "Tobias", age = 30, city = "Bergen")
+
+def my_function(title, *args, **kwargs):
+  print("Title:", title)
+  print("Positional arguments:", args)
+  print("Keyword arguments:", kwargs)
+
+my_function("User Info", "Emil", "Tobias", age = 25, city = "Oslo") 
+
+def my_function(a, b, c):
+  return a + b + c
+
+numbers = [1, 2, 3]
+result = my_function(*numbers) # Same as: my_function(1, 2, 3)
+print(result)
+
+def my_function(fname, lname):
+  print("Hello", fname, lname)
+
+person = {"fname": "Emil", "lname": "Refsnes"}
+my_function(**person) # Same as: my_function(fname="Emil", lname="Refsnes") 
+
+
+def myfunc():
+  x = 300
+  def myinnerfunc():
+    print(x)
+  myinnerfunc()
+
+myfunc()
+
+
+"""
+Local     - Inside the current function
+Enclosing - Inside enclosing functions (from inner to outer)
+Global    - At the top level of the module
+Built-in  - In Python's built-in namespace
+"""
+
+def changecase(func):
+  def inner():
+    return func().upper()
+  return inner
+
+@changecase
+def myfunction():
+  return "hello world!"
+
+print(myfunction())
+
+
+def changecase(func):
+  def myinner():
+    return func().lower()
+  return myinner
+
+@changecase
+def myFunc():
+  return "HELLO WORLD!"
+
+print(myFunc())
+
+def changecase(func):
+  def myinner(x):
+    return func(x).upper()
+  return myinner
+
+@changecase
+def myfunction(nam):
+  return "Hello " + nam
+
+print(myfunction("John"))
+
+def changecase(func):
+  def myinner(*args, **kwargs):
+    return func(*args, **kwargs).upper()
+  return myinner
+
+@changecase
+def myfunction(nam):
+  return "Hello " + nam
+
+print(myfunction("John")) 
