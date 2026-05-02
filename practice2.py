bedafdc42a2e5c0590b6e413d494b01d5ad616e2
Adnan Jukkerwala <adnanjukker402@gmail.com>
2026-05-02 19:42:13 +0530

    practice

---
+++ practice2.py
@@ -0,0 +1,15 @@
+a = 22
+b = 13
+print(a+b)
+print(a>b)
+print(a%b)
+
+a = int(input("Enter a number 1: "))
+b = int(input("Enter a number 2: "))
+
+print ("your avg is ", (a + b) /2)
+a = int(input("Enter a number 1: "))
+print("Square of a number is : ", a*a)
+
+a = input("Enter the value of a = ")
+print(type(a))
