5f015bad6525221da6c0a7fec3f2fe0e45da4540
Adnan Jukker <93384175+AdnanJukker@users.noreply.github.com>
2026-05-12 13:16:10 +0530

    Add files via upload

---
+++ practice13.py
@@ -0,0 +1,31 @@
+# name = input("Enter the name of student: ")
+# marks = int(input("Enter the marks of student: "))
+# phno = int(input("Enter the phone number of student: "))
+
+# a = "The name of student is {},his marks are {} and his phoneno. is {}".format
+# ( name ,marks ,phno)
+# print(a)
+
+# table = [str(7*i) for i in range(1,11)]
+
+# s = "The table of 7 is: " + ", \n".join(table)
+# print(s)
+
+# def divisible5(n):
+#     if n%5 == 0:
+#         return True
+#     return False
+# a = [11,22,44,55,67,88786,87676,55,45,35,67,324,43434,5657]
+
+# f = list(filter(divisible5,a))
+# print(f)
+
+# from functools import reduce
+# l=[11,22,44,55,67,88786,87676,55,45,35,67,324,43434,5657]
+
+# def greater(a,b):
+#     if a>b:
+#         return a
+#     return b
+
+# print(reduce(greater,l))
